4dd1b26aa9d5e4285317ad5d2afb12283b0f1c4a
Marcelo Sebastián Valdez <48698148+sebasvaldez@users.noreply.github.com>
2024-10-21 01:07:07 -0300

    Creando repo y algunas carpetas

---
+++ sistema-tickets/src/TicketsApp.jsx
@@ -0,0 +1,12 @@
+
+
+function TicketsApp() {
+ 
+  return (
+    <>
+     <h1>TicketsApp</h1>
+    </>
+  )
+}
+
+export default TicketsApp
---
+++ sistema-tickets/src/tickets/pages/NoTechnicalPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NoTechnicalPage = () => {
+  return (
+    <div>NoTechnicalPage</div>
+  )
+}
