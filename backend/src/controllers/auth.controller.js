8a51bd73cb235dcc444fcfa51faa96f2b9a30bd4
Marcelo Sebastián Valdez <48698148+sebasvaldez@users.noreply.github.com>
2024-10-22 23:40:23 -0300

    Rutas de usuario completas

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,10 +29,39 @@ export const register = async (req, res) => {
       role: userSaved.role,
     });
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound)
+      return res.status(404).json({ message: "Usuario no encontrado." });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Credenciales inválidas." });
+
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      name: userFound.name,
+      lastname: userFound.lastname,
+      email: userFound.email,
+      role: userFound.role,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", { expires: new Date(0) });
+
+  return res.sendStatus(200);
 };
